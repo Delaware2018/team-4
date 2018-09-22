931911848bff6ddac56c638e84eb0fb9104f2be1
Stephan <stephanedmonson@gmail.com>
2018-09-21 23:48:19 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import './App.css';
-import { Switch, Route} from 'react-router';
+import {
+  Switch,
+  Route
+} from 'react-router';
 import Home from './Home/Home';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-              <Switch>
+    return ( <
+      div className = "App" >
+      <
+      Switch >
 
-              <Route exact path="/" component={Home}/>
+      <
+      Route exact path = "/"
+      component = {
+        Home
+      }
+      />
 
-              {/* <Route path="/" component={}/>
+      {
+        /* <Route path="/" component={}/>
 
-              <Route path="/" component={}/> */}
+                      <Route path="/" component={}/> */
+      }
 
-            </Switch>
-      </div>
+      <
+      /Switch> <
+      /div>
     );
   }
 }

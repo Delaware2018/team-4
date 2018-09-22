cd4d98d4b81576591f4bf2e1cf6230f500b97bd1
Rod Ta <rody101@gmail.com>
2018-09-22 10:52:50 -0400

    trying to combine django with reactjs

--- bpos2/mysite/mysite/urls.py
+++ bpos2/mysite/mysite/urls.py
@@ -22,9 +22,13 @@ Including another URLconf
 
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-               path('bpositive.urls/', include('bpositive.urls')),
-               path('admin/', admin.site.urls),
+               #path('bpositive.urls/', include('bpositive.urls')),
+                path('admin/', admin.site.urls),
+
+                re_path('.*', TemplateView.as_view(template_name='index.html')),
                ]

7e53a80676c5be91bd02545eef53f0f7911fcd67
Stephan <stephanedmonson@gmail.com>
2018-09-21 22:35:38 -0400

    added mdbreact, added nav component

---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Nav from '../Nav/Nav';
+
+class home extends Component{
+    render(){
+        return(
+            <Nav/>
+        );
+    }
+}
+
+
+export default home;
